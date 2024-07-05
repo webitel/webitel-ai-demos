6976538ab620fedc42975ca56d837b639a4f4412
Nazar <nazar@webitel.ua>
2024-07-05 09:47:30 +0300

    feat: chatbot evaluation automated, and human A/B(need to improve A/B)

---
+++ chatbot_rag/chatbot_evaluation/automatic_evaluator.py
@@ -0,0 +1,210 @@
+from langchain.evaluation import load_evaluator
+import langchain_core
+from langchain_openai import ChatOpenAI
+import json 
+import grpc
+import chatbot_pb2
+import chatbot_pb2_grpc
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import pandas as pd
+from collections import Counter
+import altair as alt
+import requests
+from langchain.evaluation import ExactMatchStringEvaluator
+
+class Evaluator:
+    def __init__(self,user_metadata):
+        self.user_metadata = user_metadata
+        self.channel = grpc.insecure_channel('localhost:50056')
+        self.stub = chatbot_pb2_grpc.ChatServiceStub(self.channel)    
+        self.accuracy_criteria = {
+    "accuracy": """
+You are evaluating the interactions between an assistant and a customer in the banking and credit domains. The evaluation focuses on the accuracy and relevance of the assistant's responses to the customer's queries. Please follow the criteria below to score each response.
+Score 1: The answer is completely unrelated to the reference and to the question, or the answer is completely inaccurate.
+Score 3: The answer has minor relevance but does not align with the reference.
+Score 5: The answer has moderate relevance but contains inaccuracies.
+Score 7: The answer aligns with the reference but has minor errors or omissions.
+Score 10: The answer is completely accurate and aligns perfectly with the reference.
+
+Important notes:
+All questions/inputs are related to banking, credits domain.
+The assistant's responses should be evaluated as if they are part of a customer support chat.
+Be aware that the reference answer is not always 100% correct and may sometimes contain errors.
+"""
+}
+        
+        self.evaluator = load_evaluator("labeled_score_string",criteria=self.accuracy_criteria ,llm=ChatOpenAI(model="gpt-4o",temperature=0))
+        self.embedding_evaluator = load_evaluator("embedding_distance", embeddings=Embedding())
+        self.exact_match_evaluator = ExactMatchStringEvaluator(
+            ignore_case=True,
+            ignore_numbers=True,
+            ignore_punctuation=True,
+        )
+
+    def load_evaluation_data(self, data : str  | bytes):
+        """
+        data - (str) path to the data file.
+        """
+        if isinstance(data,str):
+            data = open(data).read()
+        elif isinstance(data,bytes):
+            data = data.decode('utf-8')
+        old = '\\\\'
+        new = '\\'
+        data_new = data.replace('//','/')
+        data_new = data_new.replace(old,new)
+        evaluation_data = json.loads(data_new)[:1]
+        print(evaluation_data[0])
+        self.evaluation_data =  self.process_evaluation_data(evaluation_data)
+        print(f"Loaded {len(evaluation_data)} evaluation data.")
+        print(self.evaluation_data[0])
+        
+        return evaluation_data
+    
+    
+    def process_evaluation_data(self, evaluation_data):
+        processed_data = []
+    
+    
+        #merge messages
+        for chat in evaluation_data:
+            # chat_id = chat['id']
+            messages = chat['messages']
+            processed_data.append(merge_messages(messages))
+
+        return processed_data
+        
+
+
+    def generate_answer(self, chat_history):
+        if len(chat_history) > 6:
+            chat_history = chat_history[-6:]
+        response = self.stub.Answer(chatbot_pb2.MessageRequest(user_metadata=self.user_metadata, categories=['all'], messages=chat_history))
+        return response.response_message
+
+
+    def evaluate_answer(self,user_question,generated_answer,reference_answer ):
+        # Correct
+        eval_result = self.evaluator.evaluate_strings(
+            prediction=generated_answer,
+            reference=reference_answer,
+            input=user_question,
+        )
+        print(user_question, generated_answer, reference_answer, sep='\n')
+        print(eval_result)
+        eval_result['exact_match'] = self.exact_match_evaluator.evaluate_strings(prediction=generated_answer,reference=reference_answer)
+        eval_result['embedding_distance'] = self.embedding_evaluator.evaluate_strings(prediction=generated_answer,reference=reference_answer)
+        return eval_result
+
+    def evaluate(self):
+        results = []
+        for chat in tqdm(self.evaluation_data):
+            chat_history = []
+            for message in chat:
+                if message['operator'] and chat_history:
+                    text = self.generate_answer(chat_history)
+                    #TODO add eval here
+                    evaluation_result = self.evaluate_answer(user_question =chat_history[-1].message,generated_answer=text, reference_answer=message['text'])
+                    
+                    results.append({'user_question':chat_history[-1].message,'generated_answer':text,'reference_answer':message['text'],'score':evaluation_result['score'], 'reasoning':evaluation_result['reasoning'], 'exact_match':evaluation_result['exact_match'], 'embedding_distance':evaluation_result['embedding_distance']})
+                elif message['operator'] and not chat_history:
+                    #skip the first message of operator if it is the first message (we will have there greeting message)
+                    pass
+                else:
+                    text = message['text']
+                {'sender': 'ai' if message['operator'] else 'human','text':text}
+                proto_message = chatbot_pb2.Message(sender='ai' if message['operator'] else 'human',message=text)
+                chat_history.append(proto_message)
+        
+        plot_data = []
+        exact_match_data = []
+        embedding_distance_data = []
+        # Plot the results
+        for result in results:
+            plot_data.append(result['score'])
+            exact_match_data.append(result['exact_match'])
+            embedding_distance_data.append(result['embedding_distance'])
+
+        pie_chart_scores = create_bar_plot(plot_data, title='Distribution of Scores',x_title="Score")
+        pie_chart_exact_match = create_bar_plot(exact_match_data, title='Distribution of Scores',x_title="Exact Match")
+        chart_embedding_scores = alt.Chart(pd.DataFrame({'Embedding Distance': embedding_distance_data})).mark_boxplot().encode(
+            y=alt.Y('Embedding Distance:Q', title='Embedding Distance')
+        ).properties(
+            title='Box Plot of Embedding Distances'
+        )
+        pie_chart_scores.display()
+        pie_chart_exact_match.display()
+        chart_embedding_scores.display()
+        return pie_chart_scores, pie_chart_exact_match,chart_embedding_scores
+
+
+
+def create_bar_plot(data, title, x_title):
+    df = pd.DataFrame({'Value': data})
+    bar_chart = alt.Chart(df).transform_aggregate(
+        count='count()', groupby=['Value']
+    ).mark_bar().encode(
+        x=alt.X('Value:O', title=x_title),
+        y=alt.Y('count:Q', title='Count'),
+        tooltip=['Value', 'count:Q']
+    ).properties(
+        title=title
+    )
+    return bar_chart
+
+
+def merge_messages(data):
+    merged_data = []
+    current_user = None
+    data = [item for item in data if item['name'] != 'bot']
+    operator_said_greeting = False
+    processed_data = []
+    for item in data:
+        if operator_said_greeting:
+            processed_data.append(item)
+        if item['operator'] == True:
+            operator_said_greeting = True
+        
+    for item in processed_data:
+            
+        if current_user and item['name'] == current_user['name']:
+            current_user['text'] += ' ' + item['text']  # Merge texts
+        else:
+            if current_user:
+                merged_data.append(current_user)
+            current_user = item
+
+    # Append the last user's messages
+    if current_user:
+        merged_data.append(current_user)
+    return merged_data
+
+class Embedding(langchain_core.embeddings.embeddings.Embeddings):
+    def __init__(self):
+        pass
+    
+    def embed_query(self, text : str):
+        response = requests.post('http://localhost:8000/embeddings/',json={'text':text})
+        return response.json()['embedding']  
+    
+    def embed_documents(self, texts: list[str]):
+        embeddings = []
+        for text in texts:
+            embeddings.append(self.embed_query(text))
+        return embeddings
+    
+if __name__ == "__main__":
+    embedding_evaluator = load_evaluator("embedding_distance", embeddings=Embedding())
+    res = embedding_evaluator.evaluate_strings(prediction= "Привіт",reference = "Добрий день")
+    print(res)
+    
+    exact_match_evaluator = ExactMatchStringEvaluator(
+            ignore_case=True,
+            ignore_numbers=True,
+            ignore_punctuation=True,
+        )
+    
+    res = exact_match_evaluator.evaluate_strings(prediction= "Привіт",reference = "Добрий день")
+    print(res)
---
+++ chatbot_rag/chatbot_evaluation/ui.py
@@ -0,0 +1,103 @@
+import gradio as gr
+import pandas as pd
+import random
+import json 
+
+from automatic_evaluator import Evaluator
+
+evaluator = Evaluator(user_metadata={"ContractNumber": "№12345",
+                "DueDate": "2024-05-13",
+                "StartDate": "2024-01-13",
+                "TotalDebt": "14.000 грн",
+                "credit_body": "10.000 грн",
+                "restructurization_paycheck": "300 грн",
+                "restructurization_payments_payed": "2",
+                "total_restructurization_payments": "24",
+                "ApplicationStatus (статус заявки)": "Прийнято на розгляд, відповідь буде надіслана на пошту",
+                "PromocodeStatus": "Промокод відправлений на SMS",#"На жаль, зараз немає діючого промокоду на знижку.\\nРішення про надання знижки приймається автоматично системою.\\nСлідкуйте за СМС повідомленнями, можливо саме Вам надійде наступний промокод на отримання кредиту зі знижкою.",
+                "Auth Code (Код для входу)":"Відправлений",
+                "user_age": "22 years"}
+)
+
+# data_path="../data/raw/conversations_6m.json"
+# def upload_json(file):
+#     evaluator.load_file(file)
+    
+def process_history(chat_history):
+    res = []
+    for i,message in enumerate(chat_history):
+        if 2*i >= len(chat_history):
+            print('break')
+            break
+        message_1 = f'{chat_history[2*i]["sender"]}:\n'  +  chat_history[2*i]['message']
+        try:
+            message_2 = f'{chat_history[2*i+1]["sender"]}:\n' +   chat_history[2*i+1]['message']
+        except:
+            message_2 = ""
+        res.append((message_1, message_2))
+    return res    
+
+results_of_evalutaion = {}
+# Load the chat histories from the data source
+model_A_history = json.load(open('../data/outputs/my_results.json'))
+model_B_history = json.load(open('../data/outputs/my_results.json'))
+
+
+assert len(model_A_history) == len(model_B_history), "The number of chat histories should be the same for both models."
+
+all_indicies = list(range(len(model_A_history)))
+used_indicies = [0]
+
+
+def save_choice(choice):
+    
+    global used_indicies, chatbot_a, chatbot_b
+    results_of_evalutaion[used_indicies[-1]] = choice
+    pd.DataFrame(results_of_evalutaion.items(), columns=["Chat History Index", "Choice"]).to_csv("evaluation_results.csv", index=False)
+    
+    if len(all_indicies) == len(used_indicies):
+        # print("All chat histories have been evaluated.")
+        gr.Info("All chat histories have been evaluated.")
+        return [[("","")],[("","")]]
+    
+    index_of_chat_history = random.choice(list(set(all_indicies).difference(used_indicies)))
+    used_indicies.append(index_of_chat_history)
+    print(model_A_history[index_of_chat_history])
+    chatbot_a_chat_history = process_history(model_A_history[index_of_chat_history])
+    chatbot_b_chat_history = process_history(model_B_history[index_of_chat_history])
+    
+    return [chatbot_a_chat_history,chatbot_b_chat_history]
+
+
+with gr.Blocks() as demo:
+    with gr.Tab("A/B Evaluation"):
+        with gr.Row():
+            chatbot_a = gr.Chatbot(process_history(model_A_history[0]),label="LLM A")
+            chatbot_b = gr.Chatbot(process_history(model_B_history[0]),label="LLM B")
+            
+        feedback_text = gr.Textbox(label="Feedback", interactive=False)
+        with gr.Row():
+            btn_left_better = gr.Button("Left Better")
+            btn_right_better = gr.Button("Right Better")
+            btn_both_bad = gr.Button("Both Bad")
+            btn_both_good = gr.Button("Both Good")
+
+        btn_left_better.click(lambda: save_choice("A Better"), inputs=[], outputs=[ chatbot_a, chatbot_b])
+        btn_right_better.click(lambda: save_choice("B Better"), inputs=[], outputs=[ chatbot_a, chatbot_b])
+        btn_both_bad.click(lambda: save_choice("Both Bad"), inputs=[], outputs=[ chatbot_a, chatbot_b])
+        btn_both_good.click(lambda: save_choice("Both Good"), inputs=[], outputs=[ chatbot_a, chatbot_b])
+    
+    with gr.Tab("Automated Evaluation"):
+        eval_btn = gr.Button("Evaluate")
+        with gr.Row():
+            score_plot = gr.Plot(label="Score Plot")
+            exact_match_plot = gr.Plot(label="EM Plot")
+            embedding_plot = gr.Plot(label="Embedding Plot")
+            
+        eval_btn.click(evaluator.evaluate, outputs=[score_plot,exact_match_plot,embedding_plot])
+        u = gr.UploadButton("Upload JSON with conversations",file_count='single', type = 'binary')
+        u.upload(evaluator.load_evaluation_data,[u])
+    
+        # gr.Textbox("Evaluation Results", text="Evaluation Results will be displayed here.", interactive=False)
+
+demo.launch()
